d970ac19b93d77801a9394f6d2595f0426340dad
lbarry9 <laurabarry9@hotmail.co.uk>
2025-03-15 16:17:50 +0100

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Home() {
+  const [] = useState(0)
+  const projects = [
+    {
+      name: 'Project 1',
+      description: 'This is a project',
+      link: 'https://42.fr'
+    },
+    {
+      name: 'Project 2',
+      description: 'This is another project',
+      link: 'https://afafp.fr'
+    },
+    {
+      name: 'Project 3',
+      description: 'This is a third project',
+      link: 'https://devolution-web.fr'
+    },
+  ]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1 className="text-2xl text-orange-700">Portfolio</h1>
+     {/* menu(nav) img? */}
+     <main>
+      <section>
+        <h2>About Me</h2>
+      </section>
+      <section>
+        <h2>Projects</h2>
+        {projects.map((project, index) => (
+          <div key={index}>
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+            <a href={project.link}>Visit</a>
+          </div>
+        ))}
+      </section>
+      <section>
+        <h2>Contact</h2>
+      </section>
+     </main>
     </>
   )
 }
 
-export default App
+export default Home
